47c3a93691ba0a1a784c4ccd8e1d5d0339a943fa
Celmar Pereira <celmarpereiradeandrade@gmail.com>
2026-06-01 12:15:30 -0300

    Encapsulamento Parte 1

--- exercicios/ex008/ex008.py
+++ exercicios/ex008/ex008.py
@@ -4,31 +4,31 @@ class ContaBancaria:
     """
 
     def __init__(self, id, nome, saldo = 0):
-        self.id = id
-        self.titular = nome
-        self.saldo = saldo
+        self.id = id  # publico (+)
+        self._titular = nome  # protegido (#)
+        self.__saldo = saldo  # privado (-)
 
-        print(f"Conta {self.id} criada com sucesso. Saldo atual de R${self.saldo:,.2f}.")
+        print(f"Conta {self.id} criada com sucesso. Saldo atual de R${self.__saldo:,.2f}.")
 
     def __str__(self):
-        return f"A conta {self.id} de {self.titular} tem R${self.saldo:,.2f} de saldo."
+        # return f"A conta {self.id} de {self._titular} tem R${self.__saldo:,.2f} de saldo."
+
+        return f"Estado atual da conta: {self.__dict__}"
 
     def depositar(self, valor):
-        self.saldo += valor
+        valor = abs(valor)
+
+        self.__saldo += valor
 
         print(f"Depósito de R${valor:,.2f} autorizado na conta {self.id}.")
 
     def sacar(self, valor):
-        if valor > self.saldo:
+        valor = abs(valor)
+
+        if valor > self.__saldo:
             print(f"Saque NEGADO de R${valor:,.2f}  na conta {self.id}: SALDO INSUFICIENTE!")
 
         else:
-            self.saldo -= valor
+            self.__saldo -= valor
 
             print(f"Saque de R${valor:,.2f} autorizado na conta {self.id}.")
-
-
-c1 = ContaBancaria(122, "Gustavo", 3000)
-c1.depositar(500)
-c1.sacar(2_000_000)
-print(c1)
