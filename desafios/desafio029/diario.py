0893d3b2c8afd97ae610b8eac5bbfd77ea4787fd
CelmarPA <celmarpereiradeandrade@gmail.com>
2026-06-29 14:59:40 -0300

    Correção desafio029

--- desafios/desafio029/diario.py
+++ desafios/desafio029/diario.py
@@ -5,11 +5,23 @@ class Diario:
 
     def __init__(self, senha):
         self.__segredos = []
-        self.__senha = senha
+        self.__senha = senha.strip()
 
     @property
     def senha(self):
-        return PermissionError("Ninguém tem permissão de ver a senha")
+        raise PermissionError("Ninguém tem permissão de ver a senha")
+
+    @senha.setter
+    def senha(self, nova_senha):
+        senha_atual = input("Informe sua senha atual: ")
+
+        if senha_atual == self.__senha:
+            self.__senha = nova_senha
+
+            print("A senha foi alterada com sucesso.")
+
+        else:
+            raise PermissionError("Senha inválida...")
 
     def escrever(self, msg):
         self.__segredos.append(msg)
